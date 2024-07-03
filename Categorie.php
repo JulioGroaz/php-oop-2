a1b27b2bd7693d5384d4723df9434efd527ef815
JulioGroaz <julio.groaz2020@gmail.com>
2024-07-03 16:18:57 +0200

    Creata la classe categoria (commentata )

--- Categorie.php
+++ Categorie.php
@@ -1,4 +1,43 @@
 <?php
+class Categorie {
+    private $nome;
+    private $icon;
 
+    /**
+     *  function: construct for the class Categoria 
+     *
+     * @param [string] $nome
+     * @param [string] $icon
+     */
+    public function __construct($nome, $icon){
+        $this -> nome = $nome;
+        $this -> icon = $icon;
+    }
+
+    /**
+     *  function return name 
+     *
+     * @return string name of the category
+     */
+
+    public function getNome(){
+        return $this->nome;
+    }
+
+    /**
+     *  function return the icon of the category
+     *
+     * @return string icon of the category
+     */
+    public function getIcon(){
+        return $this->icon;
+    }
+
+
+    
+
+
+
+}
 
 ?>
