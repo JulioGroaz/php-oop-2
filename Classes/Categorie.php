3482ca44ee3b96c785ee42d71febf5d268f9f361
JulioGroaz <julio.groaz2020@gmail.com>
2024-07-03 17:23:06 +0200

    creata la classe prodotti

--- Classes/Categorie.php
+++ Classes/Categorie.php
@@ -33,11 +33,6 @@ class Categorie {
         return $this->icon;
     }
 
-
-    
-
-
-
 }
 
 ?>
--- Classes/Prodotti.php
+++ Classes/Prodotti.php
@@ -1,4 +1,46 @@
 <?php
+class Prodotti {
+    private $titolo;
+    private $prezzo;
+    private $immagine;
+    private $categoria;
+    private $tipo;
 
+    /**
+     *  function construct for the class products
+     *
+     * @param [string] $titolo
+     * @param [int] $prezzo
+     * @param [string] $immagine
+     * @param [string] $categoria
+     * @param [string] $tipo
+     */
+    public function __construct($titolo, $prezzo, $immagine, $categoria, $tipo) {
+        $this->titolo = $titolo;
+        $this->prezzo = $prezzo;
+        $this->immagine = $immagine;
+        $this->categoria = $categoria;
+        $this->tipo = $tipo;
+    }
 
+    public function getTitolo() {
+        return $this->titolo;
+    }
+
+    public function getPrezzo() {
+        return $this->prezzo;
+    }
+
+    public function getImmagine() {
+        return $this->immagine;
+    }
+
+    public function getCategoria() {
+        return $this->categoria;
+    }
+
+    public function getTipo() {
+        return $this->tipo;
+    }
+}
 ?>
